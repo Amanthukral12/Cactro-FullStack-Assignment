eca98ae71684a94b763067721af3b0f753ae05d0
Aman Thukral <thukral618@gmail.com>
2025-03-16 11:53:37 +0530

    Setup express server and util files for API response and error

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = 8000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
