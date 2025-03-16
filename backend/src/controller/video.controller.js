13a344ec9133cc209f5b9b85e9c36fd925ec9ced
Aman Thukral <thukral618@gmail.com>
2025-03-16 15:03:13 +0530

    Add controller functions for add and delete comments

--- backend/src/controller/video.controller.js
+++ backend/src/controller/video.controller.js
@@ -3,6 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import axios from "axios";
 const VIDEO_ID = "qF0xOowhAnE";
+const CHANNEL_ID = "UC72pnSp-3vdjYv2owUMzlHQ";
 export const getVideo = asyncHandler(async (req, res) => {
   const id = req.params.id;
   const result = await axios.get(`${process.env.YOUTUBE_API_URI}/videos`, {
@@ -51,4 +52,63 @@ export const updateTitle = asyncHandler(async (req, res) => {
     },
   });
   return res.status(200).json(new ApiResponse(200, {}, "Title updated"));
+});
+
+export const addComment = asyncHandler(async (req, res) => {
+  const comment = req.body.comment;
+  const result = await axios.post(
+    `${process.env.YOUTUBE_API_URI}/commentThreads?part=snippet`,
+    {
+      snippet: {
+        videoId: VIDEO_ID,
+        channelId: CHANNEL_ID,
+        topLevelComment: {
+          snippet: {
+            textOriginal: comment,
+          },
+        },
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.YOUTUBE_ACCESS_TOKEN}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const commentId = result.data.id;
+  await prisma.eventLog.create({
+    data: {
+      action: "COMMENT_ADDED",
+      videoId: VIDEO_ID,
+      commentId,
+    },
+  });
+  return res.status(201).json(new ApiResponse(201, commentId, "Comment Added"));
+});
+
+export const deleteComment = asyncHandler(async (req, res) => {
+  const commentId = req.params.commentId;
+  try {
+    await axios.delete(
+      `${process.env.YOUTUBE_API_URI}/comments?id=${commentId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.YOUTUBE_ACCESS_TOKEN}`,
+        },
+      }
+    );
+    await prisma.eventLog.create({
+      data: { action: "COMMENT_DELETED", videoId: VIDEO_ID, commentId },
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Comment deleted successfully"));
+  } catch (error) {
+    console.error(
+      "Error deleting comment:",
+      error.response?.data || error.message
+    );
+  }
 });
--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { getVideo, updateTitle } from "../controller/video.controller.js";
+import {
+  addComment,
+  deleteComment,
+  getVideo,
+  updateTitle,
+} from "../controller/video.controller.js";
 const router = Router();
 
 router.route("/:id").get(getVideo);
 router.route("/updateTitle").put(updateTitle);
+router.route("/addComment").post(addComment);
+router.route("/comment/:commentId").delete(deleteComment);
 
 export default router;
